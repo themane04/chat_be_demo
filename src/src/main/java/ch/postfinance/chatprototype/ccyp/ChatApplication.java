f1ee4ca07583349df97981ed46a3a3a7552d6a7c
Marjan Tomev <marjan.tomev.1@postfinance.ch>
2024-10-10 12:05:04 +0200

    backend starts

---
+++ src/src/main/java/ch/postfinance/chatprototype/ccyp/ChatApplication.java
@@ -0,0 +1,18 @@
+package ch.postfinance.chatprototype.ccyp;
+
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "1m")
+@EnableConfigurationProperties
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
