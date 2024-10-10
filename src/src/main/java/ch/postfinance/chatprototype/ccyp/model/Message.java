110b3b41d4c2e1db1ea628a7b0258b02688a25c3
Marjan Tomev <marjan.tomev.1@postfinance.ch>
2024-10-10 15:56:59 +0200

    extended model

--- src/src/main/java/ch/postfinance/chatprototype/ccyp/model/Message.java
+++ src/src/main/java/ch/postfinance/chatprototype/ccyp/model/Message.java
@@ -3,16 +3,32 @@ package ch.postfinance.chatprototype.ccyp.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.Map;
+
 @Setter
 @Getter
 public class Message {
+    private Long id;
     private String content;
     private String sender;
     private MessageType type;
+    private Date createdAt;
+    private Date updatedAt;
+    private MessageStatus status;
+    private Map<String, Integer> reactions;
+    private Long replyToMessageId;
+    private boolean isEdited;
 
     public enum MessageType {
         CHAT,
         JOIN,
         LEAVE
+    }
+
+    public enum MessageStatus {
+        SENT,
+        DELIVERED,
+        READ
     }
 }
