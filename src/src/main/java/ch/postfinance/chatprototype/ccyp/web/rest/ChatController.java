2fb8b483057353ab0729dc3e3273ca3c849c0073
mane <tomevmarjan33@gmail.com>
2025-01-28 19:07:37 +0100

    added leave User

--- src/src/main/java/ch/postfinance/chatprototype/ccyp/web/rest/ChatController.java
+++ src/src/main/java/ch/postfinance/chatprototype/ccyp/web/rest/ChatController.java
@@ -2,6 +2,7 @@ package ch.postfinance.chatprototype.ccyp.web.rest;
 
 import ch.postfinance.chatprototype.ccyp.model.Message;
 import ch.postfinance.chatprototype.ccyp.model.enums.MessageType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -9,11 +10,14 @@ import org.springframework.stereotype.Controller;
 
 import java.util.Objects;
 
+@Slf4j
 @Controller
 public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public Message sendMessage(Message chatMessage) {
+        chatMessage.setType(MessageType.CHAT);
+        log.info("Message with id: '{}' and content: '{}' was sent", chatMessage.getId(), chatMessage.getContent());
         return chatMessage;
     }
 
@@ -22,6 +26,18 @@ public class ChatController {
     public Message addUser(Message chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chatMessage.getSender());
         chatMessage.setType(MessageType.JOIN);
+        chatMessage.setContent(chatMessage.getSender() + " joined the chat");
+        log.info("User '{}' joined the chat", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.leaveUser")
+    @SendTo("/topic/public")
+    public Message leaveUser(Message chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chatMessage.getSender());
+        chatMessage.setType(MessageType.LEAVE);
+        chatMessage.setContent(chatMessage.getSender() + " left the chat");
+        log.info("User '{}' left the chat", chatMessage.getSender());
         return chatMessage;
     }
 }
