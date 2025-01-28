100918cac521a3c9712d1211d17231409f1495a1
mane <tomevmarjan33@gmail.com>
2025-01-28 18:30:00 +0100

    initial commit

--- src/src/main/java/ch/postfinance/chatprototype/ccyp/ChatApplication.java
+++ src/src/main/java/ch/postfinance/chatprototype/ccyp/ChatApplication.java
@@ -3,16 +3,16 @@ package ch.postfinance.chatprototype.ccyp;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "1m")
 @EnableConfigurationProperties
 public class ChatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
     }
